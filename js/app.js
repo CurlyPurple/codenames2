12078528d25c49b546fdc519ac5df0e848d75ea2
AlexG <Alexanderg.savitr@gmail.com>
2024-07-29 16:43:58 -0500

    Clean code

--- js/app.js
+++ js/app.js
@@ -26,170 +26,170 @@ toggleBtn.addEventListener('click', switchUsers)
 
 init()
 function init() {
-    losingGuesses = []
-    winningGuesses = []
-    currentGuesses = []
-    wordsForGame = []
-    winningWords = []
-    bystanderWords = []
-    assassinWords = []
-    cardEls.forEach((card, idx) => {
-        cardEls[idx].textContent = ''
-        cardEls[idx].style.backgroundColor = "white";
-    })
-    if (playAgainBtn.textContent === "Play Again?") {
-        playAgainBtn.textContent = "START"
-        playAgainBtn.removeEventListener('click', init)
-        playAgainBtn.addEventListener('click', start)
-    }
-    userRole = "SPYMASTER"
-    toggleBtn.textContent = "SPYMASTER"
+losingGuesses = []
+winningGuesses = []
+currentGuesses = []
+wordsForGame = []
+winningWords = []
+bystanderWords = []
+assassinWords = []
+cardEls.forEach((card, idx) => {
+  cardEls[idx].textContent = ''
+  cardEls[idx].style.backgroundColor = "white";
+})
+  if (playAgainBtn.textContent === "Play Again?") {
+    playAgainBtn.textContent = "START"
+    playAgainBtn.removeEventListener('click', init)
+    playAgainBtn.addEventListener('click', start)
+  }
+userRole = "SPYMASTER"
+toggleBtn.textContent = "SPYMASTER"
 }
 
 function start() {
-    if (playAgainBtn.textContent === "START") {
+  if (playAgainBtn.textContent === "START") {
     playAgainBtn.innerHTML = "Play Again?"
     playAgainBtn.removeEventListener('click', start)
     playAgainBtn.addEventListener('click', init)
-    }
-    pullGameWords()
-    assignGameWords()
-    displayGameWords()
-    switchUsers()
-    cardAction()
+  }
+pullGameWords()
+assignGameWords()
+displayGameWords()
+switchUsers()
+cardAction()
 }
 
 function pullGameWords() {
-    while (wordsForGame.length < 16) {
-        let randomIndex = Math.floor(Math.random() * wordPool.length)
-        if (!wordsForGame.includes(wordPool[randomIndex])) {
-            wordsForGame.push(wordPool[randomIndex])
-        }
+  while (wordsForGame.length < 16) {
+    let randomIndex = Math.floor(Math.random() * wordPool.length)
+    if (!wordsForGame.includes(wordPool[randomIndex])) {
+      wordsForGame.push(wordPool[randomIndex])
     }
+  }
 }
 
 function assignGameWords() {
-    while (winningWords.length + bystanderWords.length + assassinWords.length < 16) {
-        let randomIndex = Math.floor(Math.random() * wordsForGame.length)
-        if (!winningWords.includes(wordsForGame[randomIndex]) && winningWords.length < 8) {
-            winningWords.push(wordsForGame[randomIndex])
-        } else if (!bystanderWords.includes(wordsForGame[randomIndex]) && !winningWords.includes(wordsForGame[randomIndex]) && bystanderWords.length < 7) {
-            bystanderWords.push(wordsForGame[randomIndex])
-        } else if (!winningWords.includes(wordsForGame[randomIndex]) && !bystanderWords.includes(wordsForGame[randomIndex])) {
-            assassinWords.push(wordsForGame[randomIndex])
-        }
-    }
+  while (winningWords.length + bystanderWords.length + assassinWords.length < 16) {
+    let randomIndex = Math.floor(Math.random() * wordsForGame.length)
+    if (!winningWords.includes(wordsForGame[randomIndex]) && winningWords.length < 8) {
+      winningWords.push(wordsForGame[randomIndex])
+    } else if (!bystanderWords.includes(wordsForGame[randomIndex]) && !winningWords.includes(wordsForGame[randomIndex]) && bystanderWords.length < 7) {
+        bystanderWords.push(wordsForGame[randomIndex])
+    } else if (!winningWords.includes(wordsForGame[randomIndex]) && !bystanderWords.includes(wordsForGame[randomIndex])) {
+        assassinWords.push(wordsForGame[randomIndex])
+      }
+  }
 }
 
 function displayGameWords() {
-    cardEls.forEach((card, idx) => {
-        cardEls[idx].textContent = wordsForGame[idx]
-        if (winningWords.includes(wordsForGame[idx])) {
-            cardEls[idx].style.backgroundColor = "#3E6024";
-            cardEls[idx].style.color = "white";
-        } else if (bystanderWords.includes(wordsForGame[idx])) {
-            cardEls[idx].style.backgroundColor = "#B1160B";
-            cardEls[idx].style.color = "white";
-         } else if (assassinWords.includes(wordsForGame[idx])) {
-            cardEls[idx].style.backgroundColor = "#2B2D42"
-            cardEls[idx].style.color = "white";
-        }
-     })
+  cardEls.forEach((card, idx) => {
+    cardEls[idx].textContent = wordsForGame[idx]
+    if (winningWords.includes(wordsForGame[idx])) {
+      cardEls[idx].style.backgroundColor = "#3E6024";
+      cardEls[idx].style.color = "white";
+    } else if (bystanderWords.includes(wordsForGame[idx])) {
+      cardEls[idx].style.backgroundColor = "#B1160B";
+      cardEls[idx].style.color = "white";
+    } else if (assassinWords.includes(wordsForGame[idx])) {
+      cardEls[idx].style.backgroundColor = "#2B2D42"
+      cardEls[idx].style.color = "white";
     }
-     
+  })
+}
+
 function switchUsers() {
-    if (userRole === "SPYMASTER") {
-        userRole = "GUESSER"
-        toggleBtn.textContent = `${userRole}`
-        guesserRole()
-    } else if (userRole === "GUESSER") {
-        userRole = "SPYMASTER"
-        toggleBtn.textContent = `${userRole}`
-        spymasterRole()
-    }
+  if (userRole === "SPYMASTER") {
+    userRole = "GUESSER"
+    toggleBtn.textContent = `${userRole}`
+    guesserRole()
+  } else if (userRole === "GUESSER") {
+      userRole = "SPYMASTER"
+      toggleBtn.textContent = `${userRole}`
+      spymasterRole()
+  }
 }
 
 function spymasterRole() {
-        cardEls.forEach((card, idx) => {
-            cardEls[idx].textContent = wordsForGame[idx]
-            if (winningWords.includes(wordsForGame[idx])) {
-                cardEls[idx].style.backgroundColor = "#3E6024";
-                cardEls[idx].style.color = "white";
-            } else if (bystanderWords.includes(wordsForGame[idx])) {
-                cardEls[idx].style.backgroundColor = "#B1160B";
-                cardEls[idx].style.color = "white";
-             } else if (assassinWords.includes(wordsForGame[idx])) {
-                cardEls[idx].style.backgroundColor = "#2B2D42"
-                cardEls[idx].style.color = "white";
-            }
-         })
+  cardEls.forEach((card, idx) => {
+  cardEls[idx].textContent = wordsForGame[idx]
+  if (winningWords.includes(wordsForGame[idx])) {
+    cardEls[idx].style.backgroundColor = "#3E6024";
+    cardEls[idx].style.color = "white";
+  } else if (bystanderWords.includes(wordsForGame[idx])) {
+    cardEls[idx].style.backgroundColor = "#B1160B";
+    cardEls[idx].style.color = "white";
+  } else if (assassinWords.includes(wordsForGame[idx])) {
+    cardEls[idx].style.backgroundColor = "#2B2D42"
+    cardEls[idx].style.color = "white";
     }
+  })
+}
 
 function guesserRole() {
-        cardEls.forEach((card, idx) => {
-            if (!currentGuesses.includes(idx)) {
-            cardEls[idx].textContent = wordsForGame[idx]
-            cardEls[idx].style.backgroundColor = "white";
-            cardEls[idx].style.color = "black";
-            } else if (currentGuesses.includes(idx) && bystanderWords.includes(wordsForGame[idx])) {
-                    cardEls[idx].style.backgroundColor = "#B1160B";
-                    cardEls[idx].style.color = "white";
-            } else if (currentGuesses.includes(idx) && winningWords.includes(wordsForGame[idx])) {
-                    cardEls[idx].style.backgroundColor = "#3E6024";
-                    cardEls[idx].style.color = "white";
-            } else if (currentGuesses.includes(idx) && assassinWords.includes(wordsForGame[idx])) {
-                    cardEls[idx].style.backgroundColor = "#2B2D42"
-                    cardEls[idx].style.color = "white"
-            }
-        })
+  cardEls.forEach((card, idx) => {
+    if (!currentGuesses.includes(idx)) {
+      cardEls[idx].textContent = wordsForGame[idx]
+      cardEls[idx].style.backgroundColor = "white";
+      cardEls[idx].style.color = "black";
+    } else if (currentGuesses.includes(idx) && bystanderWords.includes(wordsForGame[idx])) {
+      cardEls[idx].style.backgroundColor = "#B1160B";
+      cardEls[idx].style.color = "white";
+    } else if (currentGuesses.includes(idx) && winningWords.includes(wordsForGame[idx])) {
+      cardEls[idx].style.backgroundColor = "#3E6024";
+      cardEls[idx].style.color = "white";
+    } else if (currentGuesses.includes(idx) && assassinWords.includes(wordsForGame[idx])) {
+      cardEls[idx].style.backgroundColor = "#2B2D42"
+      cardEls[idx].style.color = "white"
     }
+  })
+}
 
 function cardAction() {
-    cardEls.forEach((card, idx) => {
-        cardEls[idx].addEventListener('click', pickResult)
-    })
+  cardEls.forEach((card, idx) => {
+    cardEls[idx].addEventListener('click', pickResult)
+  })
 }
 
 function pickResult(event) {
-    pickedCard = parseInt(event.target.id)
-    if (assassinWords.includes(wordsForGame[pickedCard])) {
-        cardEls[pickedCard].style.backgroundColor = "#2B2D42";
-        cardEls[pickedCard].textContent = "Game Over!"
-        cardEls[pickedCard].style.color = "white";
-        currentGuesses.push(pickedCard)
-        cardEls.forEach((card, idx) => {
-            cardEls[idx].removeEventListener('click', pickResult)
-        })
-    } else if (bystanderWords.includes(wordsForGame[pickedCard])) {
-        cardEls[pickedCard].style.backgroundColor = "#B1160B";
-        cardEls[pickedCard].style.color = "white";
-        currentGuesses.push(pickedCard)
-        losingGuesses.push(pickedCard)
-        loser()
-    } else if (winningWords.includes(wordsForGame[pickedCard])) {
-        cardEls[pickedCard].style.backgroundColor = "#3E6024";
-        cardEls[pickedCard].style.color = "white";
-        currentGuesses.push(pickedCard)
-        winningGuesses.push(pickedCard)
-        winner()
+  pickedCard = parseInt(event.target.id)
+  if (assassinWords.includes(wordsForGame[pickedCard])) {
+    cardEls[pickedCard].style.backgroundColor = "#2B2D42";
+    cardEls[pickedCard].textContent = "Game Over!"
+    cardEls[pickedCard].style.color = "white";
+    currentGuesses.push(pickedCard)
+    cardEls.forEach((card, idx) => {
+      cardEls[idx].removeEventListener('click', pickResult)
+    })
+  } else if (bystanderWords.includes(wordsForGame[pickedCard])) {
+    cardEls[pickedCard].style.backgroundColor = "#B1160B";
+    cardEls[pickedCard].style.color = "white";
+    currentGuesses.push(pickedCard)
+    losingGuesses.push(pickedCard)
+    loser()
+  } else if (winningWords.includes(wordsForGame[pickedCard])) {
+    cardEls[pickedCard].style.backgroundColor = "#3E6024";
+    cardEls[pickedCard].style.color = "white";
+    currentGuesses.push(pickedCard)
+    winningGuesses.push(pickedCard)
+    winner()
 
-    }
+  }
 }
 
 function loser() {
-    if (userRole === "GUESSER" && losingGuesses.length > 3) {
-        cardEls.forEach((card, idx) => {
-        cardEls[idx].removeEventListener('click', pickResult)
-        cardEls[idx].textContent = "Game Over!"
-        })
-    }
+  if (userRole === "GUESSER" && losingGuesses.length > 3) {
+    cardEls.forEach((card, idx) => {
+      cardEls[idx].removeEventListener('click', pickResult)
+      cardEls[idx].textContent = "Game Over!"
+    })
+  }
 }
 
 function winner() {
-    if (userRole === "GUESSER" && winningGuesses.length > 7) {
+  if (userRole === "GUESSER" && winningGuesses.length > 7) {
     cardEls.forEach((card, idx) => {
-    cardEls[idx].removeEventListener('click', pickResult)
-    cardEls[idx].textContent = "Winner!"
+      cardEls[idx].removeEventListener('click', pickResult)
+      cardEls[idx].textContent = "Winner!"
     })
-}
+  }
 }
